940c5f65064ce35126bedb311e3425b474ea23dd
Martín Cigorraga <cig0.github@gmail.com>
2024-07-14 21:52:34 -0300

    Add Instacrap.

--- nixos/modules/networking/stevenblack-unblacklist.nix
+++ nixos/modules/networking/stevenblack-unblacklist.nix
@@ -8,7 +8,7 @@
     wantedBy = [ "multi-user.target" ];
     serviceConfig = {
       Type = "oneshot";
-      ExecStart = "${pkgs.gnused}/bin/sed -i -e '/reddit/d' -e '/whatsapp/d' -e '/linkedin/d' -e '/licdn.com/d' /etc/hosts";
+      ExecStart = "${pkgs.gnused}/bin/sed -i -e '/reddit/d' -e '/whatsapp/d' -e '/linkedin/d' -e '/licdn.com/d' -e '/instagram.com/d' /etc/hosts";
     };
   };
 }
